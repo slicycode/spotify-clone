dc8ead7bc783f17cc965c53cedc0b1fccfd6db52
slicycode <deparisjules180@gmail.com>
2023-05-31 22:48:56 +0200

    Added Supabase db + auth and upload songs modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,42 @@
-import Sidebar from "@/components/Sidebar";
-import "./globals.css";
 import { Figtree } from "next/font/google";
 
-const figtree = Figtree({ subsets: ["latin"] });
+import Sidebar from "@/components/Sidebar";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+// import Player from '@/components/Player'
+
+import "./globals.css";
+import ToasterProvider from "@/providers/ToastProvider";
+
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Spotify Clone",
-  description: "Listen to music!",
+  description: "Spotify Clone",
 };
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // const products = await getActiveProductsWithPrices();
+  // const userSongs = await getSongsByUserId();
+
   return (
     <html lang="en">
-      <body className={figtree.className}>
-        <Sidebar>{children}</Sidebar>
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+            {/* <Player /> */}
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
